ba69e01c0f6ade6d177468eb6e90ea5a0c862a2e
renankrz <renankrz@gmail.com>
2022-05-14 18:52:11 -0300

    implement class criterioDeAprovacao

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,15 +1,22 @@
 package domain
 
+import domain.criterios.CriterioDeAprovacao
+
 class AnalisadorDeAprovacao {
 
-    // ---------------------------------
-    //
-    //      Seu código deve vir aqui
-    //
-    //      Defina atributos e métodos conforme especificado
-    //      no arquivo de teste encontrado em
-    //      'src/test/kotlin/domain/AnalisadorDeAprovacaoTest'
-    //
-    // ---------------------------------
+    private lateinit var criterioDeAprovacao: CriterioDeAprovacao
+
+    fun defineCriterio(c: CriterioDeAprovacao) {
+        this.criterioDeAprovacao = c
+    }
+
+    fun fechaBoletim(b: Boletim): BoletimFechado {
+        return BoletimFechado(
+            b.mediaEPs,
+            b.mediaMiniEPs,
+            criterioDeAprovacao.mediaFinal(b),
+            criterioDeAprovacao.estaAprovado(b)
+        )
+    }
 
 }
